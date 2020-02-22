6e8b60f69e736606a3f49e6f20b0c65745ab81ce
SongJungwoo <ijacsong98@gmail.com>
2020-02-22 18:53:39 +0900

    Make API entry for CRUD and ReadAll
    I made PostService and PostsApiController for CRUD and ReadAll functions.
    So now you can add, read, update, delete and read all some posts

---
+++ src/main/java/docs/web/PostsApiController.java
@@ -0,0 +1,44 @@
+package docs.web;
+
+import docs.service.PostsService;
+import docs.web.dto.PostsListResponseDto;
+import docs.web.dto.PostsResponseDto;
+import docs.web.dto.PostsSaveRequestDto;
+import docs.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+}
